7a15b9bbde91f87d2dd9f68c174d0eed0c372393
amalkrishna <amalkrishna263@outlook.com>
2021-11-25 17:48:21 +0530

    Add food item categories

---
+++ Web/CanteenManager.Web/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace CanteenManager.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<FoodItem> FoodItems { get; set; }
+    }
+}
