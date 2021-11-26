4357257c1acac53cfd7973b8b51361c330fb4c82
amalkrishna <amalkrishna263@outlook.com>
2021-11-26 08:39:47 +0530

    Add user registration

---
+++ Web/CanteenManager.Web/Migrations/20211125160903_AddUserSubtypes.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CanteenManager.Web.Migrations
+{
+    public partial class AddUserSubtypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CartId",
+                table: "OrderItems",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CartId",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_CartId",
+                table: "OrderItems",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CartId",
+                table: "AspNetUsers",
+                column: "CartId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Carts_CartId",
+                table: "AspNetUsers",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Carts_CartId",
+                table: "OrderItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Carts_CartId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Carts_CartId",
+                table: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_CartId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CartId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "AspNetUsers");
+        }
+    }
+}
